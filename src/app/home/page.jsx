01dff9c2a3e81283b9585f47a3c92541324c21e9
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-10-01 20:18:01 -0300

    ..

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -7,26 +7,26 @@ export default function Home() {
             <div className={styles.values}>
                 <div className={styles.section}>
                     <div className={styles.diamond}>
-                        <Image className={styles.icon} width={100} height={100} src={"/values/certificado.png"}/>
+                        <Image className={styles.icon} width={100} height={100} src={"/home/certificado.png"} alt={"Icon Valores"}/>
                     </div>
                     <p className={styles.p}>Qualidade Certificada</p>
                 </div>
                 <div className={styles.section}>
                     <p className={styles.p}>Atendimento Personalizado</p>
                     <div className={styles.diamond}>
-                        <Image className={styles.icon} width={90} height={90} src={"/values/person.png"}/>
+                        <Image className={styles.icon} width={90} height={90} src={"/home/person.png"} alt={"Icon Valores"}/>
                     </div>
                 </div>
                 <div className={styles.section}>
                     <div className={styles.diamond}>
-                        <Image className={styles.icon} width={100} height={100} src={"/values/seguranca.png"}/>
+                        <Image className={styles.icon} width={100} height={100} src={"/home/seguranca.png"} alt={"Icon Valores"}/>
                     </div>
                     <p className={styles.p}>Compromisso <br /> com a <br /> Segurança</p>
                 </div>
             </div>
 
             <div className={styles.products}>
-                
+
             </div>
         </>
     )
