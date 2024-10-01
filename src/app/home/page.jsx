4686c1eda55b50e5904ced60bbfd131820edfd21
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-10-01 20:09:10 -0300

    values

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import styles from './Home.module.css'
+
 export default function Home() {
     return (
         <>
             <div className={styles.values}>
+                <div className={styles.section}>
+                    <div className={styles.diamond}>
+                        <Image className={styles.icon} width={100} height={100} src={"/values/certificado.png"}/>
+                    </div>
+                    <p className={styles.p}>Qualidade Certificada</p>
+                </div>
+                <div className={styles.section}>
+                    <p className={styles.p}>Atendimento Personalizado</p>
+                    <div className={styles.diamond}>
+                        <Image className={styles.icon} width={90} height={90} src={"/values/person.png"}/>
+                    </div>
+                </div>
+                <div className={styles.section}>
+                    <div className={styles.diamond}>
+                        <Image className={styles.icon} width={100} height={100} src={"/values/seguranca.png"}/>
+                    </div>
+                    <p className={styles.p}>Compromisso <br /> com a <br /> Segurança</p>
+                </div>
+            </div>
+
+            <div className={styles.products}>
                 
             </div>
         </>
