023c0d3cd31b88d73eea47973829b09fbe2b69bc
Pedro Alves <pedro.alves6491@gmail.com>
2024-10-02 23:34:40 -0300

    Ajustes
    Fiz alguns ajustes porque estava bugando na hora de colocar o footer, então arrumei algumas coisinhas.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,19 +8,19 @@ import logoimg from '../../../public/about/img1.jpeg'
 export default function About() {
     return (
         <>
-
-        <h1 className={styles.title}>Quem somos?</h1>
-        <div className={styles.imageContainer}>
-            <div className={styles.diamondLayout}>
-                <Image src={logoimg} width={300} height={300} alt="Diamond Image" className={styles.diamondImage}/>
+        <div className={styles.mainContent}>
+            <h1 className={styles.title}>Quem somos?</h1>
+            <div className={styles.imageContainer}>
+                <div className={styles.diamondLayout}>
+                    <Image src={logoimg} width={300} height={300} alt="Diamond Image" className={styles.diamondImage}/>
+                </div>
+            </div>
+            <div className={styles.pAboutContainer}>
+                <p className={styles.pAbout}>Atuamos desde 2006 na cidade de Manhuaçu/MG, fruto de uma visão  empreendedora, ímpar de seus idealizadores, nasceu a Fábrica  Telaminas. Ao longo dos anos sempre na busca constante de sua própria superação, a empresa hoje se enquadra como um dos maiores  fabricantes de Telas e Alambrados do país.</p>
+                <p className={styles.pAbout}>A Telaminas apresenta soluções sob medida para as suas necessidades em  cercamento. Sempre com atendimento personalizado, desenvolve  trabalhos focados  em estabelecer uma parceria duradoura, para que junto com você, seja  possível realizar boas ações. Por esse e outros motivos, hoje a Telaminas  é conceituada no mercado de telas com foco total na qualidade e  excelência de seus produtos.</p>
+                <p className={styles.pAbout}>Procuramos melhorar continuamente nossos serviços para que nossos  clientes sintam-se satisfeitos e possam sempre contar com nosso  suporte.</p>
             </div>
         </div>
-        <div className={styles.pAboutContainer}>
-            <p className={styles.pAbout}>Atuamos desde 2006 na cidade de Manhuaçu/MG, fruto de uma visão  empreendedora, ímpar de seus idealizadores, nasceu a Fábrica  Telaminas. Ao longo dos anos sempre na busca constante de sua própria superação, a empresa hoje se enquadra como um dos maiores  fabricantes de Telas e Alambrados do país.</p>
-            <p className={styles.pAbout}>A Telaminas apresenta soluções sob medida para as suas necessidades em  cercamento. Sempre com atendimento personalizado, desenvolve  trabalhos focados  em estabelecer uma parceria duradoura, para que junto com você, seja  possível realizar boas ações. Por esse e outros motivos, hoje a Telaminas  é conceituada no mercado de telas com foco total na qualidade e  excelência de seus produtos.</p>
-            <p className={styles.pAbout}>Procuramos melhorar continuamente nossos serviços para que nossos  clientes sintam-se satisfeitos e possam sempre contar com nosso  suporte.</p>
-        </div>
-
         
         </>
 
