b098ac7aab6679fa701c0007773435187b6522d3
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-10-17 17:45:15 -0300

    update in home & nv update

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -53,7 +53,7 @@ export default function Home() {
             {/* Section Products */}
 
             <div className={styles.products}>
-                <h1 className={styles.h1_product}> <Image className={styles.tela_icon} src={"/home/alambrado.png"} height={60} width={50}/> <Image className={styles.tela_icon_pc} src={"/home/alambrado.png"} height={80} width={70}/> Produtos</h1>
+                <h1 className={styles.h1_product}> <Image className={styles.tela_icon} src={"/home/alambrado.png"} height={60} width={50} /> <Image className={styles.tela_icon_pc} src={"/home/alambrado.png"} height={80} width={70} /> Produtos</h1>
                 <div className={styles.section_card}>
                     <Image data-aos="fade-up" className={styles.img_product} height={400} width={350} src={"/example/card3.jpeg"} />
                     <Image data-aos="fade-up" className={styles.img_product} height={400} width={350} src={"/example/card2.jpeg"} />
@@ -63,7 +63,7 @@ export default function Home() {
                     <Image data-aos="flip-right" className={styles.img_product_pc} height={450} width={400} src={"/example/card2.jpeg"} />
                     <Image data-aos="flip-right" className={styles.img_product_pc} height={450} width={400} src={"/example/card1.jpeg"} />
                 </div>
-                <button data-aos="fade-up" className={styles.btn_product}>Ver Todos</button>
+                <button className={styles.btn_product}>Ver Todos</button>
             </div>
 
             {/* Section About Us */}
@@ -93,8 +93,43 @@ export default function Home() {
 
             {/* Section Testimonials */}
 
+            <div className={styles.testimonials_pc}>
+                <h1 className={styles.h1_product}> <Image className={styles.tela_icon} src={"/home/alambrado.png"} height={60} width={50} /> <Image className={styles.tela_icon_pc} src={"/home/alambrado.png"} height={80} width={70} /> Testemunhos</h1>
+
+                <div className={styles.section_cards_testimonials}>
+
+                    <div className={styles.cardWtxt}>
+                        <div data-aos="flip-left" className={styles.testimonials_card}>
+                            <Image className={styles.img_testimonials} height={400} width={700} src={'/home/pvc_tes.png'} />
+                        </div>
+                        <div className={styles.text_testimonials}>
+                            <p data-aos='fade-left' className={styles.txt_testimonials}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab possimus sed iusto voluptas exercitationem aut sapiente vero molestias odio vel esse neque ex ducimus itaque fugiat beatae quibusdam, veritatis at!</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.cardWtxt}>
+                        <div className={styles.text_testimonials}>
+                            <p data-aos='fade-right' className={styles.txt_testimonials}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab possimus sed iusto voluptas exercitationem aut sapiente vero molestias odio vel esse neque ex ducimus itaque fugiat beatae quibusdam, veritatis at!</p>
+                        </div>
+                        <div data-aos="flip-right" className={styles.testimonials_card}>
+                            <Image className={styles.img_testimonials} height={400} width={700} src={'/home/protecao_tes.jpeg'} />
+                        </div>
+                    </div>
+
+                    <div className={styles.cardWtxt}>
+                        <div data-aos="flip-left" className={styles.testimonials_card}>
+                            <Image className={styles.img_testimonials} height={400} width={700} src={'/home/alambrado_tes.png'} />
+                        </div>
+                        <div className={styles.text_testimonials}>
+                            <p data-aos='fade-left' className={styles.txt_testimonials}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab possimus sed iusto voluptas exercitationem aut sapiente vero molestias odio vel esse neque ex ducimus itaque fugiat beatae quibusdam, veritatis at!</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
             <div className={styles.testimonials}>
-                <h1 className={styles.h1_product}> <Image src={"/home/alambrado.png"} height={60} width={50} /> Testemunhos</h1>
+                <h1 className={styles.h1_product}> <Image className={styles.tela_icon} src={"/home/alambrado.png"} height={60} width={50} /> <Image className={styles.tela_icon_pc} src={"/home/alambrado.png"} height={80} width={70} /> Testemunhos</h1>
 
                 <div className={styles.section_cards_testimonials}>
                     <div data-aos="flip-left" className={styles.testimonials_card}>
@@ -117,8 +152,10 @@ export default function Home() {
                 </div>
 
                 <div className={styles.brands_img}>
-                    <Image height={60} width={90} src={'/home/belgo.png'} />
-                    <Image height={60} width={120} src={'/home/telaminas.png'} />
+                    <Image className={styles.img_brand} height={60} width={90} src={'/home/belgo.png'} />
+                    <Image className={styles.img_brand} height={60} width={120} src={'/home/telaminas.png'} />
+                    <Image className={styles.img_brand_pc} height={120} width={180} src={'/home/belgo.png'} />
+                    <Image className={styles.img_brand_pc} height={120} width={240} src={'/home/telaminas.png'} />
                 </div>
             </div>
 
@@ -149,6 +186,30 @@ export default function Home() {
                             <Image className={styles.img_qr} height={120} width={120} src={'/home/qr.png'} />
                         </div>
                     </div>
+                </div>
+
+                <div data-aos="fade-up" className={styles.container_qr}>
+                    <div className={styles.text}>
+                        <p className={styles.qr_p}>Sua opinião é importante! <br /> Avalie-nos no Google.</p>
+                        <p className={styles.qr_p}> Escaneie o QR code <br /> ou <br /> Clique Aqui</p>
+                    </div>
+                    <div className={styles.container_img_qr}>
+                        <Image className={styles.img_qr} height={200} width={200} src={'/home/qr.png'} />
+                    </div>
+                </div>
+            </div>
+            <div className={styles.informations_pc}>
+                <div data-aos="fade-up" className={styles.informations_item}>
+                    <Image height={80} width={80} src={'/home/local_icon.png'} />
+                    <p className={styles.informations_p}>Av. Pres. Tancredo Neves, 891 - Santa Terezinha, Manhuaçu - MG, 36904-076</p>
+                </div>
+                <div data-aos="fade-up" className={styles.informations_item}>
+                    <Image height={80} width={80} src={'/home/time_icon.png'} />
+                    <p className={styles.informations_p}>Segunda a Sexta: 7:30 - 18:00 <br />Sábado: 7:30 - 12:00</p>
+                </div>
+                <div data-aos="fade-up" className={styles.informations_item_zap}>
+                    <Image height={80} width={80} src={'/home/zap_icon.png'} />
+                    <p className={styles.informations_p}>+55 (33)3332-1740</p>
                 </div>
             </div>
 
