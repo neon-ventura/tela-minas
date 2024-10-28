b1c468c13c1edff8c45b69def2bc8093e7724d3e
Pedro Alves <pedro.alves6491@gmail.com>
2024-10-28 13:54:51 -0300

    Produto PC

--- src/app/components/productmodel/ProductModel.jsx
+++ src/app/components/productmodel/ProductModel.jsx
@@ -3,23 +3,21 @@ import '@/app/globals.css'
 import styles from './ProductModel.module.css'
 import Image from 'next/image.js'
 import logoimg from '../../../../public/about/img1.jpeg'
-import Nav from '../nav/Nav'
-import Footer from '../footer/Footer'
 
 export default function ProductModel(){
     return (
         <>
 
             <div className={styles.main}>
-                <h2 className={styles.productName}>Nome do Produto</h2>
+                <h2 className={styles.productName}>Nome do Produto Nome do Produto Nome do Produto</h2>
                 <div className={styles.diamond}>
                     <Image src={logoimg} width={300} height={300} alt="Diamond Image" className={styles.imgDiamond} />
                 </div>
                 <p className={styles.description}>A Concertina possui formato espiralizado composta por lâminas de
                 aço cortantes, bem afiadas e pontiagudas.</p>
                 
-                <div className={styles.carcteristicas}>
-                    <h6>Principais Caracteristicas</h6>
+                <div className={styles.caracteristicas}>
+                    <h6 className={styles.caracListTitle}>Principais Caracteristicas</h6>
                     <ul className={styles.caracList}>
                         <li className={styles.caracListItem}>Melhor custo benefício;</li>
                         <li className={styles.caracListItem}>Não necessita de energia elétrica;</li>
