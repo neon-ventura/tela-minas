5e6709f3b52ffd3eddb18e37771a2b58c41d4618
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-10-28 20:57:28 -0300

    products atualizados

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -71,49 +71,130 @@ export default function Products() {
                 </div>
                 <h1 className={styles.title}>Todos</h1>
                 <div className={styles.card}>
-                   <Image className={styles.img} src={'/products/alambrado.avif'} width={150} height={230}/>
-                    <div>
-                        <p className={styles.title_product}>Alambrado Galvanizado</p>
+                   <Image className={styles.img} src={'/products/alambradoG.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product_alam}>Alambrado Galvanizado</p>
                         <button className={styles.btn1}>Comprar</button>
                         <button className={styles.btn2}>Sobre</button>
                     </div>
                 </div>
                 <div className={styles.card}>
-                   <Image className={styles.img} src={'/products/revestido.jpg'} width={150} height={230}/>
-                    <div>
+                   <Image className={styles.img} src={'/products/revestido.jpg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
                         <p className={styles.title_product}>Alambrado Revestido</p>
                         <button className={styles.btn1}>Comprar</button>
                         <button className={styles.btn2}>Sobre</button>
                     </div>
                 </div>
                 <div className={styles.card}>
-                   <Image className={styles.img} src={'/products/galinheiro.jpg'} width={150} height={230}/>
-                    <div>
-                        <p className={styles.title_product}>Hexagonal Galinheiro</p>
+                   <Image className={styles.img} src={'/products/gradil.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Gradil</p>
                         <button className={styles.btn1}>Comprar</button>
                         <button className={styles.btn2}>Sobre</button>
                     </div>
                 </div>
                 <div className={styles.card}>
-                   <Image className={styles.img} src={'/products/galinheiro.jpg'} width={150} height={230}/>
-                    <div>
-                        <p className={styles.title_product}>Hexagonal Pinteiro</p>
+                   <Image className={styles.img} src={'/products/hexagonal.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Hexagonal</p>
                         <button className={styles.btn1}>Comprar</button>
                         <button className={styles.btn2}>Sobre</button>
                     </div>
                 </div>
+                
                 <div className={styles.card}>
-                   <Image className={styles.img} src={'/products/galinheiro.jpg'} width={150} height={230}/>
-                    <div>
-                        <p className={styles.title_product}>Hexagonal Viveiro</p>
-                        <button className={styles.btn1}>Comprar</button>
-                        <button className={styles.btn2}>Sobre</button>
-                    </div>
-                </div>
-                <div className={styles.card}>
-                   <Image className={styles.img} src={'/products/soldada.webp'} width={150} height={230}/>
-                    <div>
+                   <Image className={styles.img} src={'/products/soldada.webp'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
                         <p className={styles.title_product}>Soldada</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/ondulada.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Ondulada</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/mosquiteiro.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Mosqueteiro</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/sombreamento.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Sombreamento</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/moeda.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Chapa Moeda</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/Concertina.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Concertina</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/expandida.png'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Chapa Expandida</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/peneira.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Peneira</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/rede.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Rede de Proteção</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/arame-liso.webp'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Arame Liso</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/arame-farpado.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Arame Farpado</p>
+                        <button className={styles.btn1}>Comprar</button>
+                        <button className={styles.btn2}>Sobre</button>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                   <Image className={styles.img} src={'/products/acessorios.jpeg'} width={300} height={300}/>
+                    <div className={styles.txt_section}>
+                        <p className={styles.title_product}>Acessórios</p>
                         <button className={styles.btn1}>Comprar</button>
                         <button className={styles.btn2}>Sobre</button>
                     </div>
